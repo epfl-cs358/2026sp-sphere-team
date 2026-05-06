3c691bd0dd6fd24e2667c513a7fb6f445be54cf7
Alessandro Lombardini <113979061+ahlombardini@users.noreply.github.com>
2026-05-06 19:09:11 +0200

    feat: add OmniDrivetrain with tests
    Concrete Drivetrain<BodyVelocity> wiring OmniKinematics + 3 PID
    loops. MockMotor for isolated testing.

---
+++ code/body-firmware/src/drivetrain/OmniDrivetrain.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "Drivetrain.h"
+#include "OmniKinematics.h"
+#include "PID.h"
+#include "BodyVelocity.h"
+#include "DrivetrainConfig.h"
+
+class OmniDrivetrain : public Drivetrain<BodyVelocity> {
+public:
+    OmniDrivetrain(Motor& m0, Motor& m1, Motor& m2,
+                   const DrivetrainConfig& config,
+                   PID& pid0, PID& pid1, PID& pid2)
+        : Drivetrain(m0, m1, m2)
+        , _kinematics(config)
+        , _pids{&pid0, &pid1, &pid2} {}
+
+    void drive(const BodyVelocity& velocity) override {
+        auto rpms = _kinematics.toWheelRPMs(velocity);
+        for (int i = 0; i < 3; i++) {
+            _targetRPMs[i] = rpms[i];
+        }
+    }
+
+    void update(float dt) {
+        for (auto* m : _motors) m->update();
+
+        float s0 = _pids[0]->compute(_targetRPMs[0], _motors[0]->getFilteredRPM(), dt);
+        float s1 = _pids[1]->compute(_targetRPMs[1], _motors[1]->getFilteredRPM(), dt);
+        float s2 = _pids[2]->compute(_targetRPMs[2], _motors[2]->getFilteredRPM(), dt);
+
+        setMotorSpeeds(s0, s1, s2);
+    }
+
+    void update() override {
+        update(0.01f);
+    }
+
+    void stop() override {
+        for (auto* m : _motors) m->brake();
+        for (auto* p : _pids) p->reset();
+        for (int i = 0; i < 3; i++) _targetRPMs[i] = 0.0f;
+    }
+
+private:
+    void setMotorSpeeds(float s0, float s1, float s2) {
+        _motors[0]->setSpeed(s0);
+        _motors[1]->setSpeed(s1);
+        _motors[2]->setSpeed(s2);
+    }
+
+    OmniKinematics _kinematics;
+    PID* _pids[3];
+    float _targetRPMs[3] = {};
+};
