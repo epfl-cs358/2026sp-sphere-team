a0a4f5e003077b45a582496edb7d10e4e15a1032
Alessandro Lombardini <113979061+ahlombardini@users.noreply.github.com>
2026-05-07 17:02:18 +0200

    fix motor driver and encoder pin assignments for correct direction
    Swap motors 1&2 driver pins and all encoder A/B pins so positive
    PID output produces positive encoder RPM on all three motors.

--- code/body-firmware/src/config/pins.h
+++ code/body-firmware/src/config/pins.h
@@ -10,12 +10,12 @@
 #include "FIT0186.h"
 
 inline constexpr L298NPins MOTOR0_PINS = {.fwd = 14, .rev = 27};
-inline constexpr L298NPins MOTOR1_PINS = {.fwd = 16, .rev = 17};
-inline constexpr L298NPins MOTOR2_PINS = {.fwd = 18, .rev = 19};
+inline constexpr L298NPins MOTOR1_PINS = {.fwd = 17, .rev = 16};
+inline constexpr L298NPins MOTOR2_PINS = {.fwd = 19, .rev = 18};
 
 inline constexpr EncoderPins MOTOR0_ENCODER_PINS = {.a = 34, .b = 35};
-inline constexpr EncoderPins MOTOR1_ENCODER_PINS = {.a = 4, .b = 2};
-inline constexpr EncoderPins MOTOR2_ENCODER_PINS = {.a = 39, .b = 36};
+inline constexpr EncoderPins MOTOR1_ENCODER_PINS = {.a = 2, .b = 4};
+inline constexpr EncoderPins MOTOR2_ENCODER_PINS = {.a = 36, .b = 39};
 
 inline constexpr MotorConfig MOTOR0_ENCODER = {.encoderPins = MOTOR0_ENCODER_PINS, .encoderCPR = fit0186::ENCODER_CPR};
 inline constexpr MotorConfig MOTOR1_ENCODER = {.encoderPins = MOTOR1_ENCODER_PINS, .encoderCPR = fit0186::ENCODER_CPR};
